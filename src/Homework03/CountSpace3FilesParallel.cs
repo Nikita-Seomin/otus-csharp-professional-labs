1c559d7ea7c3295197a01002b755618e245c84a2
nicki <nickita.seomin@yandex.ru>
2025-12-16 23:29:39 +0300

    feature: add lab 03

---
+++ src/Homework03/CountSpace3FilesParallel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Concurrent;
+
+namespace Homework03;
+
+public static class CounterSpace
+{
+    public static async Task<int> Count(FileInfo file)
+    {
+        int spaceCount = 0;
+        using (FileStream fs = File.OpenRead(file.FullName))
+        {
+            int numBytesToRead;
+            var buffer = new byte[65536]; // 128 Кб
+
+            while ((numBytesToRead = await fs.ReadAsync(buffer)) > 0)
+            {
+                ReadOnlySpan<byte> span = buffer.AsSpan(0, numBytesToRead);
+
+                foreach (byte b in span)
+                {
+                    if (b == ' ') ++spaceCount;
+                }
+            }
+        }
+
+        return spaceCount;
+    }
+
+    public static async Task<Dictionary<string, int>> Count(DirectoryInfo directory)
+    {
+        if (!directory.Exists) throw new DirectoryNotFoundException();
+
+        ConcurrentDictionary<string, int> concurrentDictionary = new ConcurrentDictionary<string, int>();
+        var tasks = new List<Task>();
+
+        foreach (var file in directory.GetFiles())
+        {
+            var currentFile = file;
+            
+            tasks.Add(Task.Run(async () =>
+            {
+                try
+                {
+                    var count = await Count(currentFile);
+                    concurrentDictionary[currentFile.Name] = count;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error processing file {currentFile.Name}: {ex.Message}");
+                    concurrentDictionary[currentFile.Name] = -1;
+                }
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+        
+        return new Dictionary<string, int>(concurrentDictionary);
+    }
+}
---
+++ src/Homework03/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+
+namespace Homework03;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        #region 1 - 3 файла параллельно
+
+        var tasks = new List<Task>();
+
+        string[] arr =
+        [
+            "../../../Sample3Files/1.txt",
+            "../../../Sample3Files/2.txt",
+            "../../../Sample3Files/3.txt"
+        ];
+
+        Stopwatch stopwatch1 = new Stopwatch();
+
+        stopwatch1.Start();
+        foreach (var item in arr)
+        {
+            var currentPath = item;
+            
+            tasks.Add(Task.Run( async () =>
+            {
+                var spaceCount = await CounterSpace.Count(new FileInfo(currentPath));
+                Console.WriteLine($"В файле {currentPath} найдено {spaceCount} пробелов");
+            }));
+        }
+
+        await Task.WhenAll(tasks);
+
+        stopwatch1.Stop();
+        Console.WriteLine($"Время выполнения: {stopwatch1.ElapsedMilliseconds} мс\n");
+
+        #endregion
+
+        #region 2 - Все файлы в папке
+        Stopwatch stopwatch2 = new Stopwatch();
+        
+        stopwatch2.Start();
+        var spaceCountDic2 =  await CounterSpace.Count(new DirectoryInfo("../../../Sample3Files"));
+        stopwatch2.Stop();
+        
+        foreach (var item in spaceCountDic2)
+            Console.WriteLine($"В файле {item.Key} найдено {item.Value} пробелов");
+        
+        Console.WriteLine($"Время выполнения: {stopwatch2.ElapsedMilliseconds} мс\n");
+        #endregion
+    }
+}
